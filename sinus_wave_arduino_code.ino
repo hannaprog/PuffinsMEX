9789d2c7e48321a6533863709dd9c8c1892fb992
Gverox <157602937+Gverox@users.noreply.github.com>
2026-03-10 22:39:53 +0100

    Update sinus_wave_arduino_code.ino
    La till esc kommandon för att kunna skicka PWM signaler till thruster

--- sinus_wave_arduino_code.ino
+++ sinus_wave_arduino_code.ino
@@ -1,3 +1,7 @@
+#include <Servo.h>      // ADDED
+Servo esc;              // ADDED
+int escPin = 9;         // ADDED
+
 // Neutral and amplitude
 const int neutral = 1500;    // neutral PWM
 const int amplitude = 400;   // swing ±400 µs → 1100–1900
@@ -14,6 +18,11 @@ unsigned long startTime;
 
 void setup() {
   Serial.begin(115200);           // Initialize serial for CoolTerm
+
+  esc.attach(escPin);             // ADDED
+  esc.writeMicroseconds(neutral); // ADDED
+  delay(5000);                    // ADDED (ESC arming)
+
   delay(1000);                  // small delay before start
   startTime = micros();
   lastSampleTime = startTime;
@@ -37,6 +46,8 @@ void loop() {
         float t = (now - loopStart) / 1000000.0;       // seconds relative to this frequency
         int pwmValue = neutral + (int)(amplitude * sin(2 * 3.14159 * freq * t));
 
+        esc.writeMicroseconds(pwmValue); // ADDED
+
         // CSV output: time in seconds vs PWM
         Serial.print((now - startTime) / 1000000.0, 5); // 5 decimals for smooth plotting
         Serial.print(",");
@@ -50,6 +61,9 @@ void loop() {
       unsigned long now = micros();
       if (now - lastSampleTime >= samplePeriod) {
         lastSampleTime += samplePeriod;
+
+        esc.writeMicroseconds(neutral); // ADDED
+
         Serial.print((now - startTime) / 1000000.0, 5);
         Serial.print(",");
         Serial.println(neutral);
